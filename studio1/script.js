5e488c51aeae07e4b11c20e4a2ef1537d7245711
myuma24 <147002196+myuma24@users.noreply.github.com>
2023-10-30 02:32:19 -0700

    madlib

--- studio1/script.js
+++ studio1/script.js
@@ -6,6 +6,8 @@
 
     const madLib = document.querySelector('#madlib');
 
+    const text = document.querySelector('#text');
+
     myForm.addEventListener('submit', function(event){
         event.preventDefault();
 
@@ -64,16 +66,31 @@
             document.querySelector('#word10').focus();
         }
         else {
+            document.getElementById('overlay').className = 'showing';
+
             /* Change to over lay popup instead of just a text */
-            myText = `You sense a tickle down your spine as you get further and further into the woods. The sun is going down and the sky is getting dim really quickly. Drifting through the seemingly soundless forest, you hear a ${word1}. You turn and ${word2} in the opposite direction. Tripping against the leaves and branches you begin to fall, grabbing the nearest ${word3} you catch yourself. You gasp and breathe feeling a ${word4} grip around your ${word5}. Kicking and ${word6} the air into nothingness, you regain your position and begin ${word7} again. You spot a small cabin and crawl into the ${word8} before slamming the door shut, waiting out the night. You look outside and see ${word9} floating around with no good explanation. The spirits are here to take you you thought and your heart is ${word10}. May god have mercy on your soul.`;
+            myText = `You sense a tickle down your spine as you get further and further into the woods. The sun is going down and the sky is getting dim really quickly. Drifting through the seemingly soundless forest, you hear a <strong>${word1}</strong>. You turn and <strong>${word2}</strong> in the opposite direction. Tripping against the leaves and branches you begin to fall, grabbing the nearest <strong>${word3}</strong> you catch yourself. You gasp and breathe feeling a <strong>${word4}</strong> grip around your <strong>${word5}</strong>. Kicking and <strong>${word6}</strong> the air into nothingness, you regain your position and begin <strong>${word7}</strong> again. You spot a small cabin and crawl into the <strong>${word8}</strong> before slamming the door shut, waiting out the night. You look outside and see <strong>${word9}</strong> floating around with no good explanation. The spirits are here to take you you thought and your heart is <strong>${word10}</strong>. May god have mercy on your soul.`;
 
             const textFields = document.querySelectorAll('input[type=text]');
             for( let i=0; i<textFields.length; i++ ){
             textFields[i].value = '';}
-        }
 
-        madLib.innerHTML = myText;
+            text.innerHTML = myText;
+        }
+        document.querySelector('.close').addEventListener('click',function(event){
+            event.preventDefault();
+            document.getElementById('overlay').className = 'hidden';
+    
+        })
+    
+        document.addEventListener('keydown',function(event){
+            if (event.key === 'Escape'){
+                document.getElementById('overlay').className = 'hidden';
+            }
+        })
 
     });
+
+    
 
 })();
